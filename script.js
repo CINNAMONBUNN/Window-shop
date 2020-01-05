8a0bcfd9378633d4ac83fa4650ebabdda59c94ec
CINNAMONBUNN <sanyaman021280@gmail.com>
2020-01-06 01:58:35 +0300

    Validation of the feedback form
    Validation of the feedback form in the "Callback" section is made. Created styles for valid and invalid data.

---
+++ script.js
@@ -0,0 +1,63 @@
+const inputs = document.querySelectorAll('input[data-rule]'),    
+    textNameError = document.querySelector('#callback-name-error'),
+    textNumberError = document.querySelector('#callback-number-error'),
+    textEmailError = document.querySelector('#callback-email-error'),
+    callbackForm = document.querySelector('#callback-form');
+    
+let checking;
+
+textNameError.classList.add('valid-text');
+textNumberError.classList.add('valid-text');
+textEmailError.classList.add('valid-text');
+
+for (let input of inputs) {
+    input.addEventListener('blur', function(e) {
+        let inputRule = this.dataset.rule,
+            inputValue = this.value;
+
+        switch (inputRule) {
+            case 'name':
+                checking = /^[a-zа-яA-ZА-ЯёЁ]+$/.test(inputValue);
+                if (!checking) {
+                    textNameError.classList.add('invalid-text');
+                } else {
+                    textNameError.classList.remove('invalid-text');
+                    textNameError.classList.add('valid-text');
+                }
+                break;
+
+            case 'number':
+                checking = /^\d{11}$/.test(inputValue);
+                if (!checking) {
+                    textNumberError.classList.add('invalid-text');
+                } else {
+                    textNumberError.classList.remove('invalid-text');
+                    textNumberError.classList.add('valid-text');
+                }
+                break;
+
+            case 'email':
+                checking = /^\S+?@\S+$/.test(inputValue);
+                if (!checking) {
+                    textEmailError.classList.add('invalid-text');
+                } else {
+                    textEmailError.classList.remove('invalid-text');
+                    textEmailError.classList.add('valid-text');
+                }
+                break;
+        }
+
+        if (checking) {
+            this.classList.add('valid');
+            this.classList.remove('invalid');
+        } else {
+            this.classList.add('invalid');
+        }
+    });
+}
+
+callbackForm.addEventListener('submit', e => {
+    if (!checking) {
+        e.preventDefault();
+    }
+});
