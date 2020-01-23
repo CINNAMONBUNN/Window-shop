bb7b56d97032eb552790a33529b95e5629697215
CINNAMONBUNN <sanyaman021280@gmail.com>
2020-01-23 23:50:15 +0300

    Add validation of the second form
    Added validation of the second form in the "Consultation" section.

--- script.js
+++ script.js
@@ -1,80 +1,90 @@
-	/*form validation in the callback section*/
+	/*form validation*/
 
-const inputs = document.querySelectorAll('input[data-rule]'),    
-    textNameError = document.querySelector('#callback-name-error'),
-    textNumberError = document.querySelector('#callback-number-error'),
-    textEmailError = document.querySelector('#callback-email-error'),
-    callbackForm = document.querySelector('#callback-form');
+	const inputs = document.querySelectorAll('input[data-rule]'),
+	    textNameError = document.querySelectorAll('.name-error'),
+	    textNumberError = document.querySelectorAll('.number-error'),
+	    textEmailError = document.querySelector('.email-error'),
+	    callbackForm = document.querySelector('#callback-form'),
+	    consultationForm = document.querySelector('#consultation-form');
 
-let checking;
+	let checking;
 
-textNameError.classList.add('valid-text');
-textNumberError.classList.add('valid-text');
-textEmailError.classList.add('valid-text');
-
-for (let input of inputs) {
-    input.addEventListener('blur', function(e) {
-        let inputRule = this.dataset.rule,
-            inputValue = this.value;
-
-        switch (inputRule) {
-            case 'name':
-                checking = /^[a-zа-яA-ZА-ЯёЁ]+$/.test(inputValue);
-                if (!checking) {
-                    textNameError.classList.add('invalid-text');
-                } else {
-                    textNameError.classList.remove('invalid-text');
-                    textNameError.classList.add('valid-text');
-                }
-                break;
-
-            case 'number':
-                checking = /^\d{11}$/.test(inputValue);
-                if (!checking) {
-                    textNumberError.classList.add('invalid-text');
-                } else {
-                    textNumberError.classList.remove('invalid-text');
-                    textNumberError.classList.add('valid-text');
-                }
-                break;
-
-            case 'email':
-                checking = /^\S+?@\S+$/.test(inputValue);
-                if (!checking) {
-                    textEmailError.classList.add('invalid-text');
-                } else {
-                    textEmailError.classList.remove('invalid-text');
-                    textEmailError.classList.add('valid-text');
-                }
-                break;
-        }
-
-        if (checking) {
-            this.classList.add('valid');
-            this.classList.remove('invalid');
-        } else {
-            this.classList.add('invalid');
-        }
-    });
-}
-
-callbackForm.addEventListener('submit', e => {
-    if (!checking) {
-        e.preventDefault();
-    }
-});
+	for (let i = 0; i < 2; i++) {
+	    textNameError[i].classList.add('valid-text');
+	    textNumberError[i].classList.add('valid-text');
 
 
-/*scroll*/
+	    textEmailError.classList.add('valid-text');
 
-const navLinks = document.querySelectorAll('.nav-link');
-for(let navLink of navLinks) {
-	navLink.addEventListener('click', function(e) {
-		e.preventDefault();
-		let attrValue = $(this).attr('href');
-		$('html,body').animate({
-			scrollTop: $(attrValue).offset().top
-		}, 950)
+
+	    for (let input of inputs) {
+	        input.addEventListener('blur', function(e) {
+	            let inputRule = this.dataset.rule,
+	                inputValue = this.value;
+
+	            switch (inputRule) {
+	                case 'name':
+	                    checking = /^[a-zа-яA-ZА-ЯёЁ]+$/.test(inputValue);
+	                    if (!checking) {
+	                        textNameError[i].classList.add('invalid-text');
+	                    } else {
+	                        textNameError[i].classList.remove('invalid-text');
+	                        textNameError[i].classList.add('valid-text');
+	                    }
+	                    break;
+
+	                case 'number':
+	                    checking = /^\d{11}$/.test(inputValue);
+	                    if (!checking) {
+	                        textNumberError[i].classList.add('invalid-text');
+	                    } else {
+	                        textNumberError[i].classList.remove('invalid-text');
+	                        textNumberError[i].classList.add('valid-text');
+	                    }
+	                    break;
+
+	                case 'email':
+	                    checking = /^\S+?@\S+$/.test(inputValue);
+	                    if (!checking) {
+	                        textEmailError.classList.add('invalid-text');
+	                    } else {
+	                        textEmailError.classList.remove('invalid-text');
+	                        textEmailError.classList.add('valid-text');
+	                    }
+	                    break;
+	            }
+
+	            if (checking) {
+	                this.classList.add('valid');
+	                this.classList.remove('invalid');
+	            } else {
+	                this.classList.add('invalid');
+	            }
+	        });
+	    }
+	}
+	callbackForm.addEventListener('submit', e => {
+	    if (!checking) {
+	        e.preventDefault();
+	    }
 	});
-}
 
+	consultationForm.addEventListener('submit', e => {
+	    if (!checking) {
+	        e.preventDefault();
+	    }
+	});
+
+
+	/*scroll*/
+
+	const navLinks = document.querySelectorAll('.nav-link');
+	for (let navLink of navLinks) {
+	    navLink.addEventListener('click', function(e) {
+	        e.preventDefault();
+	        let attrValue = $(this).attr('href');
+	        $('html,body').animate({
+	            scrollTop: $(attrValue).offset().top
+	        }, 950)
+	    });
+	}
